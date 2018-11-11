136b9f6fd4ef10bec0c47c404125f8726f104794
Sasha <sashwho@vivaldi.net>
2018-11-11 13:52:10 -0800

    link uwu

---
+++ src/main/java/com/sasha/osudiscord/command/LinkCommand.java
@@ -0,0 +1,37 @@
+package com.sasha.osudiscord.command;
+
+import com.sasha.osudiscord.DiscordEventHandler;
+import com.sasha.osudiscord.LinkManager;
+import com.sasha.simplecmdsys.SimpleCommand;
+import net.dv8tion.jda.core.EmbedBuilder;
+
+import java.io.IOException;
+
+public class LinkCommand extends SimpleCommand {
+
+    public LinkCommand() {
+        super("link");
+    }
+
+    @Override public void onCommand() {
+        if (this.getArguments() == null || this.getArguments().length != 1) {
+            DiscordEventHandler.lastMessage.getChannel().sendMessage(
+                    DiscordEventHandler.Util.makeErrorEmbed("Invalid args", "Too few or too many arguments", null)
+            ).submit();
+            return;
+        }
+        try {
+            LinkManager.link(DiscordEventHandler.lastMessage.getAuthor().getId(), this.getArguments()[1]);
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.setTitle("Linked!");
+            builder.setDescription("Your osu! account has been linked.");
+            builder.setColor(0xff7cc0);
+            DiscordEventHandler.lastMessage.getChannel().sendMessage(builder.build()).submit();
+        } catch (IOException e) {
+            DiscordEventHandler.lastMessage.getChannel().sendMessage(
+                    DiscordEventHandler.Util.makeErrorEmbed("Internal server error", "An internal error occurred whilst handling your request. Please try again later.", null)
+            ).submit();
+            e.printStackTrace();
+        }
+    }
+}
