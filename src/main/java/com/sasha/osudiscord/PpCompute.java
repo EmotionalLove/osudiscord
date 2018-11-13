c0ad67b39a52fe4e9929bbeb484a23cc21dd09bf
Sasha <sashwho@vivaldi.net>
2018-11-12 16:11:59 -0800

    it works but it doesn't

--- src/main/java/com/sasha/osudiscord/PpCompute.java
+++ src/main/java/com/sasha/osudiscord/PpCompute.java
@@ -7,48 +7,43 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.net.URL;
 import java.util.UUID;
 
 public class PpCompute {
 
     private boolean calcFcSpeed;
-    private boolean calcFcAim;
+    private boolean calcFcAcc;
     private String downloadurl;
     private String filename;
     private Koohii.Map beatmap;
 
-    private double aim;
-    private double speed;
+    private double acc;
 
     public PpCompute(String downloadUrl) {
         if (!downloadUrl.toLowerCase().startsWith("https://osu.ppy.sh/osu/")) {
             throw new IllegalArgumentException("Not a valid osu beatmap download!");
         }
         this.calcFcSpeed = true;
-        this.calcFcAim = true;
+        this.calcFcAcc = true;
         this.downloadurl = downloadUrl;
         this.filename = UUID.randomUUID().toString().replace("-", "");
     }
 
-    public PpCompute(String id, double aim, double speed) {
+    public PpCompute(String id, double acc) {
         String downloadUrl = "https://osu.ppy.sh/osu/" + id;
         this.calcFcSpeed = false;
-        this.calcFcAim = false;
+        this.calcFcAcc = false;
         if (!downloadUrl.toLowerCase().startsWith("https://osu.ppy.sh/osu/")) {
-            throw new IllegalArgumentException("Not a valid osu beatmap download!");
+            throw new IllegalArgumentException("Not a valid osu beatmap ID!");
         }
-        if (aim != -1.0) {
-            this.aim = aim;
+        if (acc != -1.0) {
+            this.acc = acc;
         }else {
-            this.calcFcAim = true;
+            this.calcFcAcc = true;
         }
-        if (speed != -1.0) {
-            this.speed = aim;
-        }else {
-            this.calcFcSpeed = true;
-        }
-        this.speed = speed;
         this.downloadurl = downloadUrl;
         this.filename = UUID.randomUUID().toString().replace("-", "");
     }
@@ -58,11 +53,35 @@ public class PpCompute {
         FileUtils.copyURLToFile(new URL(downloadurl), new File(filename));
         System.out.println("Downloaded beatmap...");
         BufferedReader reader = new BufferedReader(new FileReader(new File(filename)));
-        beatmap = new Koohii.Parser().map(reader);
-        Koohii.DiffCalc stars = new Koohii.DiffCalc().calc(beatmap);
+        // calc
+        beatmap = new Koohii.Parser().map(reader); // the beatmap
+        int objects = beatmap.ncircles + beatmap.nsliders + beatmap.nspinners; // the total objects in the beatmap
+        Koohii.DiffCalc stars = new Koohii.DiffCalc().calc(beatmap); // the difficulty
         System.out.println(stars.total + " star difficulty!");
-        Koohii.PPv2 pp = new Koohii.PPv2(calcFcAim ? stars.aim : this.aim, calcFcSpeed ? stars.speed : this.speed, beatmap);
-        return pp.total;
+        Koohii.PPv2Parameters parameters = builder(beatmap, stars);
+        parameters.nmiss = objects - ((int) (objects * (acc)));
+        parameters.score_version = 1;
+        System.out.println(parameters.nmiss + " missed out of " + objects + " possible");
+        Koohii.PPv2 pp = new Koohii.PPv2(parameters); // the big UwU
+        return dround(pp.total, 3);
+    }
+    public static double dround(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(value + "");
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+    private Koohii.PPv2Parameters builder(Koohii.Map b, Koohii.DiffCalc stars) {
+        Koohii.PPv2Parameters p = new Koohii.PPv2Parameters();
+        p.n300 = b.ncircles + b.nsliders + b.nspinners;
+        p.base_ar = b.ar;
+        p.base_od = b.od;
+        p.max_combo = b.max_combo();
+        p.speed_stars = stars.speed;
+        p.aim_stars = stars.aim;
+        return p;
     }
 
     public Koohii.Map getMap() {
--- src/main/java/com/sasha/osudiscord/command/CalcCommand.java
+++ src/main/java/com/sasha/osudiscord/command/CalcCommand.java
@@ -7,7 +7,7 @@ import com.sasha.simplecmdsys.SimpleCommandInfo;
 
 import java.io.IOException;
 @SimpleCommandInfo(description = "Calculate PP for a beatmap",
-        syntax = {"<beatmap id> ['acc' <value>] ['spd' <value>]"})
+        syntax = {"<beatmap id> ['acc' <value>] ['combo' <value]"})
 public class CalcCommand extends SimpleCommand {
 
     public CalcCommand() {
@@ -16,36 +16,34 @@ public class CalcCommand extends SimpleCommand {
 
     @Override public void onCommand() {
         double acc = -1.0;
-        double spd = -1.0;
+        double combo = -1.0;
         if (this.getArguments() == null || this.getArguments().length != 1) {
             if (this.getArguments() == null) {
                 return;
             }
             if (this.getArguments().length == 3) {
                 int indexAcc = -1;
-                int indexSpd = -1;
                 for (int i = 0; i < this.getArguments().length; i++) {
                     if (this.getArguments()[i].equalsIgnoreCase("acc")) {
                         indexAcc = i;
                     }
-                    if (this.getArguments()[i].equalsIgnoreCase("spd")) {
-                        indexSpd = i;
+                    if (this.getArguments()[i].equalsIgnoreCase("combo")) {
+                        indexAcc = i;
                     }
                 }
                 if (indexAcc != -1) acc = Double.parseDouble(this.getArguments()[indexAcc + 1]) / 100;
-                if (indexSpd != -1) spd = Double.parseDouble(this.getArguments()[indexSpd + 1]) / 100;
             }
         }
         double finalAcc = acc;
-        double finalSpd = spd;
         DiscordEventHandler.lastMessage.getChannel().sendMessage(DiscordEventHandler.Util.makeGenericEmbed("Please wait...", "Downloading beatmap...")).queue(q -> {
             try {
-                PpCompute compute = new PpCompute(this.getArguments()[0], finalAcc, finalSpd);
+                PpCompute compute = new PpCompute(this.getArguments()[0], finalAcc);
                 double ppValue = compute.calc();
                 q.editMessage(DiscordEventHandler.Util.makeGenericEmbed("PP on " + compute.getMap().title, ppValue + "pp" + (finalAcc == -1.0 ? " @ FC/SS" : " @ " + finalAcc * 100 + "% accuracy"))).submit();
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (IllegalArgumentException x) {
+                x.printStackTrace();
                 q.editMessage(DiscordEventHandler.Util.makeErrorEmbed("Invalid beatmap download",
                         "Invalid beatmap download. You can only calculate PP for beatmaps hosted on the official osu website.", null)).submit();
             }
